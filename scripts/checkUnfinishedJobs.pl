72ba378f33ea01975e4e067c905b936bb0c63db9
bs674 <yifeiren07>
2020-08-15 12:20:29 -0400

    update document

--- scripts/checkUnfinishedJobs.pl
+++ scripts/checkUnfinishedJobs.pl
@@ -4,8 +4,19 @@ use strict;
 open INPUT, "$ARGV[0]";
 while( my $line=<INPUT> ){
     if( $line=~/\s(\S+)$/ ){
-        my $file = $1 . "queryo";
+        my $file = $1 . "o";
         if (-e $file) {
+            my $good = 0;
+            open IN , "$file";
+            while( my $lin = <IN> ){
+                if( $lin=~/^#done/ ){
+                    $good = 1;
+                }
+            }
+            close IN;
+            if ( 0 == $good ){
+                print "$line";
+            }
 
         }else{
             print "$line";
